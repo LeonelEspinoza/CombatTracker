672d7ffc138e5ca788f5c70b4373c6ad3f20ad1f
Leo <leonel.espinoza@ug.uchile.cl>
2026-08-30 13:49:16 -0400

    Doc & Feat: add documentation to funtions and adds current pointer to linked lists

--- entity_list.c
+++ entity_list.c
@@ -4,6 +4,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/*
+* Entity structure
+	* Name string
+	* Health
+	* Armor Class
+	* Initiative
+*/
 struct entity {
 	char* name;
 	int hp;
@@ -11,7 +18,12 @@ struct entity {
 	int initiative;
 };
 
-struct entity* createEntity(char name[], int hp, int ac, int initiative){
+/*
+* Entity Initializer
+	* Allocates memory for object
+	* Sets object's attributes with arguments values
+*/
+struct entity* createEntity(char* name, int hp, int ac, int initiative){
 	struct entity* entity = NULL;
 	entity = (struct entity*) malloc(sizeof(struct entity));
 	if(!entity){
@@ -25,17 +37,45 @@ struct entity* createEntity(char name[], int hp, int ac, int initiative){
 	return entity;
 }
 
+/*
+* Entity cleanup
+	* Free allocated memory
+	* Nullifies attributes
+*/
 void cleanupEntity(struct entity* entity){
+	entity->name = NULL;
+	entity->hp = 0;
+	entity->ac = 0;
+	entity->initiative = 0;
 	free(entity);
 	return;
 }
 
+void printEntity(struct entity* self){
+	printf(
+		"%s:\n	Health: %d\n	AC: %d\n	Initiative: %d\n",
+		self->name, self->hp, self->ac, self->initiative
+	);
+	return;
+}
+
+/*
+* Node structure
+	* Entity pointer
+	* Next Node pointer
+	* Previous Node pointer
+*/
 struct node {
 	struct entity* entity;
 	struct node* next;
 	struct node* prev;
 };
 
+/*
+* Node initializer
+	* Allocates memory for object
+	* Set entity pointer with argument value
+*/
 struct node* createNode(struct entity* entity){
 	struct node* node = NULL;
 	node = (struct node *) malloc(sizeof(struct node));
@@ -49,6 +89,12 @@ struct node* createNode(struct entity* entity){
 	return node;
 }
 
+/*
+* Node cleanup
+	* Make sure neighbors nodes are pointing eachother
+	* Free allocated memory for self and entity child
+	* Nullifies attributes
+*/
 void cleanupNode(struct node* self){
 	struct node* next = self->next;
 	struct node* prev = self->prev;
@@ -59,16 +105,11 @@ void cleanupNode(struct node* self){
 		prev->next = next;
 	}
 	cleanupEntity(self->entity);
+	self->entity = NULL;
+	self->prev = NULL;
+	self->next = NULL;
 	free(self);
 	return;
-}
-
-void setNext(struct node* self, struct node* next){
-	self->next = next;
-}
-
-void setPrev(struct node* self, struct node* prev){
-	self->prev = prev;
 }
 
 struct node* getNext(struct node* self){
@@ -83,22 +124,40 @@ struct entity* getEntity(struct node* self){
 	return self->entity;
 }
 
+/*
+* Get entity child initiative
+	* Auxiliar getter for entity child
+*/
 int getNodeInitiative(struct node* self){
 	struct entity* entity = self->entity;
 	return entity->initiative;
 }
 
 void printNode(struct node* self){
-	struct entity* entity = self->entity;
-	printf("Name: %s, HP: %d, AC: %d, Initiative: %d\n", entity->name, entity->hp, entity->ac, entity->initiative);
+	printEntity(self->entity);
 	return;
 }
 	
+/*
+* Linked list structure
+	* Sorts its members by initiative decreasing order
+	* Head: pointer first node in list
+	* Current: pointer to current node
+	* Size: number of linked nodes
+	* round: current loop number
+*/
 struct linkedList {
 	struct node* head;
+	struct node* current;
 	int size;
+	int round;
 };
 
+/*
+* Linked List Initializer
+	* Allocates memory for object
+	* Set size and round attributes to zero
+*/
 struct linkedList* initLinkedList(){
 	struct linkedList* self = NULL;
 	self = (struct linkedList*) malloc(sizeof(struct linkedList));
@@ -107,9 +166,16 @@ struct linkedList* initLinkedList(){
 		return NULL;
 	}
 	self->head = NULL;
+	self->current = NULL;
 	self->size = 0;
+	self->round = 0;
 }
 
+/*
+* Recursive Node cleanup
+	* Calls itself on next node
+	* Calls Node ceanup function on head.
+*/
 void cleanupLinkedTail(struct node* head){
 	if (head->next != NULL){
 		cleanupLinkedTail(head->next);
@@ -118,15 +184,28 @@ void cleanupLinkedTail(struct node* head){
 	return;
 }
 
+/*
+* Linked List cleanup
+	* calls recursive cleanup
+	* Nullifies object's attributes
+	* Frees allocated mamory
+*/
 void cleanupLinkedList(struct linkedList* self){
 	if (self->head != NULL){
 		cleanupLinkedTail(self->head);
 	}
+	self->head = NULL;
+	self->current = NULL;
+	self->size = 0;
+	self->round = 0;
 	free(self);
 	return;
 }
 
-
+/*
+* Recursive insert auxiliar
+	* inserts new node sorted by initiative
+*/
 void insertRecursive(struct node* head, struct node* node){
 	if (getNodeInitiative(head) < getNodeInitiative(node)){
 		if (head->prev != NULL){
@@ -147,9 +226,17 @@ void insertRecursive(struct node* head, struct node* node){
 	return;
 }
 
+/*
+* Insert ONE new node
+	* If list is empty inserts its first Node
+	* Else inserts node sorted by initiative
+	* updates head if new node is first
+	* updates list's size
+*/
 void insertNode(struct linkedList* self, struct node* node){
 	if (self->head == NULL){
 		self->head = node;
+		self->current = node;
 	} else {
 		insertRecursive(self->head,node);
 	}
@@ -163,8 +250,73 @@ void insertNode(struct linkedList* self, struct node* node){
 	return;
 }
 
+/*
+* Deletes Current Node
+	* if current is NULL do nothing
+	* if next is NULL current points to head
+	* calls node cleanup for current node
+	* updates size
+*/
+void deleteCurrentNode(struct linkedList* self){
+	struct node* cur = self->current;
+	if (cur == NULL){
+		return;
+	}
+
+	if (cur->next != NULL){
+		self->current = cur->next;
+	}else{
+		self->current = self->head;
+	}
+
+	cleanupNode(cur);
+	self->size -= 1;
+	return;
+}
+
+/*
+* Update current forward
+	* updates current pointer to next node
+	* if current is last, increase round and point to first
+*/
+void moveForward(struct linkedList* self){
+	struct node* cur = self->current;
+	if (cur->next == NULL){
+		self->current = self->head;
+		self->round += 1;
+		return;
+	}
+	self->current = cur->next;
+	return;
+}
+
+/*
+* Update current backwards
+	* updates current pointer to previous node
+	* if current is first, decrease round and point to last
+*/
+void moveBack(struct linkedList* self){
+	struct node* cur = self->current;
+	if (cur->prev == NULL && cur == self->head){
+		struct node* tmp = self->head;
+		while(tmp->next != NULL){
+			tmp=tmp->next;
+		}
+		self->current = tmp;
+		self->round -= 1;
+		return;
+	}
+
+	self->current = cur->prev;
+	return;
+}
+
+/*
+* Auxiliar recursive print
+	* prints node number and entity attributes
+*/
 void printRecursive(struct node* head, int i){
-	printf("Node #%d : ",i);
+	printf("\nNode #%d\n",i);
 	if(head->entity == NULL){
 		printf("Node without entity\n");
 	} else {
@@ -180,6 +332,7 @@ void printRecursive(struct node* head, int i){
 
 void printLinkedList(struct linkedList* list){
 	struct node* head = list->head;
+	printf("\nRound #%d | Size: %d\n", list->round, list->size);
 	printRecursive(head,1);
 	return;
 }
--- entity_list_run.c
+++ entity_list_run.c
@@ -1,15 +1,15 @@
 #include "entity_list.c"
 
 void main(){
-	struct entity p1 = {"player1", 10, 12, 5}; 
-	struct entity p2 = {"player2", 13, 10, 15}; 
-	struct entity p3 = {"player3", 12, 11, 25}; 
-	struct entity p4 = {"player4", 10, 15, 8}; 
+	struct entity* p1 = createEntity("player1", 10, 12, 5);
+	struct entity* p2 = createEntity("player2", 13, 10, 15);
+	struct entity* p3 = createEntity("player3", 12, 11, 25);
+	struct entity* p4 = createEntity("player4", 10, 15, 8);
 
-	struct node* n1 = createNode(&p1);
-	struct node* n2 = createNode(&p2);
-	struct node* n3 = createNode(&p3);
-	struct node* n4 = createNode(&p4);
+	struct node* n1 = createNode(p1);
+	struct node* n2 = createNode(p2);
+	struct node* n3 = createNode(p3);
+	struct node* n4 = createNode(p4);
 	
 	struct linkedList* l = initLinkedList();
 	insertNode(l,n1);
